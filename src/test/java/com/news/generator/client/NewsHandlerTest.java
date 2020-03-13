bf5791f8d42bf71914276bef49e82cfe3236b045
Sam Nechypurenko <semen.nechypurenko@gmail.com>
2020-03-13 12:11:14 +0200

    added dates in tests

--- src/test/java/com/news/generator/client/NewsHandlerTest.java
+++ src/test/java/com/news/generator/client/NewsHandlerTest.java
@@ -33,19 +33,21 @@ class NewsHandlerTest {
     @Test
     void shouldReturnAllNewsObjectsFromMockedExternalAPI() {
 
+        System.out.println(new Date(1584080018121L));
+
         List<Article> articles = new ArrayList<>();
         News news = new News();
 
         Article article = new Article();
         article.setTitle("Stock futures down after 11-year bull market run ended Thursday - CNN");
         article.setAuthor("Clare Duffy, CNN Business");
-        article.setPublishedAt(new Date());
+        article.setPublishedAt(new Date(1584080018121L));
         articles.add(article);
 
         article = new Article();
         article.setTitle("Head Of U.S. Soccer Resigns After Equal Pay Backlash - HuffPost");
         article.setAuthor("Nick Visser");
-        article.setPublishedAt(new Date());
+        article.setPublishedAt(new Date(1584080118121L));
         articles.add(article);
 
         news.setArticles(articles);
@@ -62,47 +64,47 @@ class NewsHandlerTest {
         Article article = new Article();
         article.setTitle("Stock futures down after 11-year bull market run ended Thursday - CNN");
         article.setAuthor("Clare Duffy, CNN Business");
-        article.setPublishedAt(new Date());
+        article.setPublishedAt(new Date(1584080010921L));
         articles.add(article);
 
         article = new Article();
         article.setTitle("'The Bachelor': Hannah Ann Sluss" +
                 " and Peter Weber's Breakup Timeline Fills in Every Red Flag - Showbiz Cheat Sheet");
         article.setAuthor("Nick Visser");
-        article.setPublishedAt(new Date());
+        article.setPublishedAt(new Date(1584080015121L));
         articles.add(article);
 
         article = new Article();
         article.setTitle("Stock futures down after 11-year bull market run ended Thursday - CNN");
         article.setAuthor("Kate Conger, David E. Sanger");
-        article.setPublishedAt(new Date());
+        article.setPublishedAt(new Date(1584080048121L));
         articles.add(article);
 
         article = new Article();
         article.setTitle("Head Of U.S. Soccer Resigns After Equal Pay Backlash - HuffPost");
         article.setAuthor("Connie Liou");
-        article.setPublishedAt(new Date());
+        article.setPublishedAt(new Date(1584080019121L));
         articles.add(article);
 
         article = new Article();
         article.setTitle("Вольфсбург – Шахтар: Каштру назвав основну перевагу" +
                 " \"гірників\" та пояснив, чому довірив пенальті Коваленку — Футбол 24 - Футбол 24");
         article.setAuthor("football24.ua");
-        article.setPublishedAt(new Date());
+        article.setPublishedAt(new Date(1584080018121L));
         articles.add(article);
 
         article = new Article();
         article.setTitle("Head Of U.S. Soccer Resigns After Equal Pay Backlash - HuffPost");
         article.setAuthor("Mark DeCambre");
-        article.setPublishedAt(new Date());
+        article.setPublishedAt(new Date(1584080218121L));
         articles.add(article);
         //news.setArticles(articles);
         articles.sort(Comparator.comparing(Article::getPublishedAt));
 
         List<Article> predictedArticleList = articles.subList(0,5);
 
-       // Mockito.when(newsHandler.getTopArticles(5)).thenReturn(articles.subList(0,5));
-       // assertEquals(predictedArticleList, newsHandler.getTopArticles(5));
+        Mockito.when(newsHandler.getTopArticles(5)).thenReturn(articles.subList(0,5));
+        assertEquals(predictedArticleList, newsHandler.getTopArticles(5));
 
 
     }
